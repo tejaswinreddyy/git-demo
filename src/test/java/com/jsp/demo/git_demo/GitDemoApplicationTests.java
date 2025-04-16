ff11104052d1509900f93f4598ef88d91e458426
Your Name <tejaswinreddy.y@gmail.com>
2025-04-16 15:08:54 +0530

    First Commit | Project Initialized

---
+++ src/test/java/com/jsp/demo/git_demo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.demo.git_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
